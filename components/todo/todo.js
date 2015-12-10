05b90fc87371880acc994ea10cd88ba165be0338
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-12-10 18:40:06 +0900

    add todos

--- components/todo/todo.js
+++ components/todo/todo.js
@@ -7,7 +7,19 @@
 
   TodoController.$inject = [];
 
+  var vm;
+
   function TodoController() {
-    console.log('TodoController Constructor');
+    vm = this;
+    vm.todoList = [];
   }
+
+  TodoController.prototype.addTodo = function () {
+    vm.todoList.push(vm.item);
+    vm.item = '';
+  };
+
+  TodoController.prototype.removeTodo = function (index) {
+    vm.todoList.splice(index, 1);
+  };
 })();
